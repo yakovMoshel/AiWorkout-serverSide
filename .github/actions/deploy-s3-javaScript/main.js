4c3fb22cc178b6ce58bf644006668388e943f778
YakovMoshel <yakovmoshel123@gmail.com>
2025-10-23 14:34:37 +0300

    setUp CI/CD workflow for server: lint, tests, and EC2 deploy

--- .github/actions/deploy-s3-javaScript/main.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
-function run(){
-    core.notice('Starting deployment to AWS S3');
-}
-
-run();
