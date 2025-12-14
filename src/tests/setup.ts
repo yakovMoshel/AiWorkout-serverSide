381285c8645b543c3b4a75c92333c60d8129766e
YakovMoshel <yakovmoshel123@gmail.com>
2025-12-14 11:06:34 +0200

    test: fix auth tests DB setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 
 let mongo: MongoMemoryServer;
 
