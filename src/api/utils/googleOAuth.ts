1975f159f3a7cc4f7afc15052fd238d79e6c8a21
YakovMoshel <yakovmoshel123@gmail.com>
2026-01-29 09:56:18 +0200

    cheack

--- src/api/utils/googleOAuth.ts
+++ src/api/utils/googleOAuth.ts
@@ -2,10 +2,13 @@ import { google } from 'googleapis';
 
 
 const redirectUri =
-  process.env.NODE_ENV === 'production'
-    ? process.env.GOOGLE_REDIRECT_URI_PROD
-    : process.env.GOOGLE_REDIRECT_URI_DEV;
+process.env.NODE_ENV === 'production'
+? process.env.GOOGLE_REDIRECT_URI_PROD
+: process.env.GOOGLE_REDIRECT_URI_DEV;
 
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('GOOGLE CLIENT ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE REDIRECT:', redirectUri);
 
 export const oauthClient = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
