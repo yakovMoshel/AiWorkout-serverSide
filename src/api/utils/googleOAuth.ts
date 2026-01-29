d95905a2605af31ce548520699cccc4dc520c278
YakovMoshel <yakovmoshel123@gmail.com>
2026-01-29 21:45:36 +0200

    CHECK: google auth

--- src/api/utils/googleOAuth.ts
+++ src/api/utils/googleOAuth.ts
@@ -6,6 +6,9 @@ export function getOAuthClient() {
       ? process.env.GOOGLE_REDIRECT_URI_PROD
       : process.env.GOOGLE_REDIRECT_URI_DEV;
 
+
+      console.log("Using redirect URI:", redirectUri);
+
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID!,
     process.env.GOOGLE_CLIENT_SECRET!,
